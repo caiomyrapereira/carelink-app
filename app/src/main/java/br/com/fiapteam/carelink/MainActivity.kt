92e57044e7ad2ef26ff5f169939031f17632d7ef
caio myra pereira <caiomyrapereira@hotmail.com>
2023-10-09 22:27:27 -0300

    add webview.

--- app/src/main/java/br/com/fiapteam/carelink/MainActivity.kt
+++ app/src/main/java/br/com/fiapteam/carelink/MainActivity.kt
@@ -2,10 +2,29 @@ package br.com.fiapteam.carelink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webview: WebView = this.findViewById(R.id.webview);
+
+        webview!!.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                url?.let {
+                    view?.loadUrl(it)
+                    return true
+                }
+                return false
+            }
+        }
+
+        webview!!.getSettings().setJavaScriptEnabled(true)
+        webview!!.loadUrl("https://carelink-frontend.vercel.app/")
     }
+
+
 }
